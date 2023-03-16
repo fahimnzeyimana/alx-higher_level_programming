bb7327e4ef9b73069fd7451e83fadaba3cd3b0aa
fahimnzeyimana <fahimnzeimana@gmail.com>
2023-03-16 11:29:15 +0000

    More returns!

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+.py
+
+
+def multiple_returns(sentence):
+    """Returns the length of a string and its first character."""
+    if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])
